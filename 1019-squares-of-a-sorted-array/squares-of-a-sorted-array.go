51d31278c59d1ccf7424593b23a0b0a64229afad
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-07-21 15:50:37 +0700

    Time: 34 ms (47.35%) | Memory: 7.1 MB (60.34%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.go
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.go
@@ -1,21 +1,11 @@
 func sortedSquares(nums []int) []int {
-        n := len(nums)
-        res := make([]int, n, n)
-        i, j := 0, n-1
-        for k := n - 1; k >= 0; k-- {
-            
-            i2 := nums[i]*nums[i]
-            j2 := nums[j]*nums[j]
-            
-            
-                if i2 > j2{
-                        res[k] = i2
-                        i++
-                } else {
-                        res[k] = j2
-                        j--
-                }
-        }
+    answer := make([]int, 0)
 
-        return res
+    for _, num := range nums {
+        sq := num*num
+        answer = append(answer, sq)
+    }
+
+    sort.Ints(answer)
+    return answer
 }
