f1cd768cb3d0925c52be31965a609e165f3bcdae
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-07-23 23:49:30 +0700

    Time: 0 ms (100.00%) | Memory: 2.1 MB (11.08%) - LeetSync

--- Day3/27-remove-element/remove-element.go
+++ Day3/27-remove-element/remove-element.go
@@ -1,24 +1,14 @@
 func removeElement(nums []int, val int) int {
-    l, r := 0, len(nums)-1
-    answer := 0
+    j := 0
 
-    for l <= r {
-        for r >= 0 && nums[r] == val {
-            r--
-            answer++
+    for i:=0;i<len(nums);i++{
+        if nums[i] != val {
+            nums[j], nums[i] = nums[i], nums[j]
+            j++
         }
-        for l < len(nums) && nums[l] != val {
-            l++
-        }
-        if l < r {
-            nums[l], nums[r] = nums[r], nums[l]
-            answer++
-        }
-        l++
-        r--
     }
 
-    return len(nums) - answer
+    return j
 }
 
 /*
