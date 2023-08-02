44cf107d5e333ac09dbce552f32f16543fe0dbff
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-02 22:00:03 +0700

    Time: 14 ms (48.72%) | Memory: 6.9 MB (87.18%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.go
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.go
@@ -9,15 +9,18 @@
 
 func convertBST(root *TreeNode) *TreeNode {
     sum := 0
-    return traverse(root, &sum)
-}
 
-func traverse(root *TreeNode, sum *int) *TreeNode {
-    if root != nil {
-        traverse(root.Right, sum)
-        *sum += root.Val
-        root.Val = *sum
-        traverse(root.Left, sum)
+    var traverse func(node *TreeNode)
+    traverse = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        traverse(node.Right)
+        sum += node.Val
+        node.Val = sum
+        traverse(node.Left)
     }
+
+    traverse(root)
     return root
 }
