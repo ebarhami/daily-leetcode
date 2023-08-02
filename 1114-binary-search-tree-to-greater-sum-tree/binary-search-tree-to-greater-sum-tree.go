19e7407af0014efaa42597ef25d12bc6c817ec78
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-02 21:57:00 +0700

    Time: 0 ms (100.00%) | Memory: 2.2 MB (100.00%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstToGst(root *TreeNode) *TreeNode {
+    sum := 0
+    return traverse(root, &sum)
+}
+
+func traverse(root *TreeNode, sum *int) *TreeNode {
+    if root != nil {
+        traverse(root.Right, sum)
+        *sum += root.Val
+        root.Val = *sum
+        traverse(root.Left, sum)
+    }
+    return root
+}
