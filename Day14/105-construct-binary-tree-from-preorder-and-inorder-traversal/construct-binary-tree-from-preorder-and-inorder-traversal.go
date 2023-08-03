88cbacf7a8052de5b07dfe54decc7a91f6f330dc
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-04 01:18:11 +0700

    Time: 7 ms (46.27%) | Memory: 4 MB (94.63%) - LeetSync

---
+++ Day14/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+    if len(preorder) == 0 {
+        return nil
+    }
+
+    idx, val := 0, preorder[0]
+    for inorder[idx] != val {
+        idx++
+    }
+
+    node := &TreeNode{
+        Val: preorder[0],
+        Left: buildTree(preorder[1:idx+1], inorder[:idx]),
+        Right: buildTree(preorder[idx+1:], inorder[idx+1:]),
+    }
+    return node
+}
+
+/*
+preorder = root + left + right
+inorder = left + root + right
+
+*/
