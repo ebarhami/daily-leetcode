afe102cccd05efa3bd705e3e2373ba1150b54a66
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-15 22:16:16 +0700

    Time: 5 ms (64.67%) | Memory: 3.2 MB (6.24%) - LeetSync

---
+++ Daily/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,32 @@
+func search(nums []int, target int) bool {
+    l, r := 0, len(nums)-1
+
+    for l <= r {
+        mid := (l+r)/2
+
+        if nums[mid] == target {
+            return true
+        }
+
+        if nums[mid] == nums[l] {
+            l++
+            continue
+        }
+
+        if nums[l] <= nums[mid] {
+            if nums[l] <= target && target <= nums[mid] {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        } else {
+            if nums[mid] <= target && target <= nums[r] {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+
+    return false
+}
