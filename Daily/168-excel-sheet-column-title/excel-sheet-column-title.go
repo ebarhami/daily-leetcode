4f145d791d26430f5f49ef7312b2ca6926dc90e0
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-22 20:44:07 +0700

    Time: 0 ms (100.00%) | Memory: 1.9 MB (94.34%) - LeetSync

--- Daily/168-excel-sheet-column-title/excel-sheet-column-title.go
+++ Daily/168-excel-sheet-column-title/excel-sheet-column-title.go
@@ -2,11 +2,11 @@ func convertToTitle(columnNumber int) string {
     answer := ""
     
     for columnNumber > 0 {
-        val := getOrderInAlphabet(columnNumber)
-        answer = fmt.Sprintf("%s%s", string(rune(int('A') + val - 1)), answer)
-        residu := 0
-        if columnNumber % 26 == 0 {residu = 1}
-        columnNumber = columnNumber/26 - residu
+        columnNumber--
+
+        answer = fmt.Sprintf("%s%s", string(rune(int('A') + columnNumber % 26)), answer)
+
+        columnNumber = columnNumber/26
     }
     return answer
 }
