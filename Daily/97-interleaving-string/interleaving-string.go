4bd4c3302593d2e4f7cc0364974beec1374e5557
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-08-26 11:39:48 +0700

    Time: 2 ms (59.69%) | Memory: 3 MB (32.56%) - LeetSync

--- Daily/97-interleaving-string/interleaving-string.go
+++ Daily/97-interleaving-string/interleaving-string.go
@@ -3,17 +3,18 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
         return false
     }
 
-    cache := make([][]int, len(s1)+1)
-    for i := 0; i <= len(s1); i++ {
-        cache[i] = make([]int, len(s2)+1)
-        for j := 0; j <= len(s2); j++ {
-            cache[i][j] = -1
+    dp := make([][]int, len(s1)+1)
+    for i:=0;i<=len(s1);i++{
+        dp[i] = make([]int, len(s2)+1)
+        for j:=0;j<=len(s2);j++{
+            dp[i][j] = -1
         }
     }
-    return isInterleaved(0, 0, 0, s1, s2, s3, cache)
+
+    return traverse(dp, 0, 0, 0, s1,s2,s3)
 }
 
-func isInterleaved(i, j, k int, s1, s2, s3 string, cache [][]int) bool {
+func traverse(dp [][]int, i, j, k int, s1,s2,s3 string) bool {
     if k == len(s3) {
         if i == len(s1) && j == len(s2) {
             return true
@@ -21,20 +22,16 @@ func isInterleaved(i, j, k int, s1, s2, s3 string, cache [][]int) bool {
         return false
     }
 
-    if cache[i][j] != -1 {
-        return cache[i][j] == 1
+    if dp[i][j] != -1 {
+        return dp[i][j] == 1
     }
 
-    cache[i][j] = 0
-    res := false
-    if i < len(s1) && s1[i] == s3[k] {
-        res = res || isInterleaved(i+1, j, k+1, s1, s2, s3, cache)
+    dp[i][j] = 0
+    if i < len(s1) && s1[i] == s3[k] && traverse(dp, i+1, j,k+1, s1,s2,s3) {
+        dp[i][j] = 1
+    } else if j < len(s2) && s2[j] == s3[k] && traverse(dp, i, j+1,k+1, s1,s2,s3) {
+        dp[i][j] = 1
     }
-    if j < len(s2) && s2[j] == s3[k] {
-        res = res || isInterleaved(i, j+1, k+1, s1, s2, s3, cache)
-    }
-    if res {
-        cache[i][j] = 1
-    }
-    return res
+
+    return dp[i][j] == 1
 }
