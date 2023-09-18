4e550230da014ef897b225988757b75a9ba67c76
ebarhami <31585147+ebarhami@users.noreply.github.com>
2023-09-18 22:32:41 +0700

    Time: 13 ms (12.99%) | Memory: 5 MB (20.78%) - LeetSync

---
+++ Daily/1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.go
@@ -0,0 +1,43 @@
+func kWeakestRows(mat [][]int, k int) []int {
+    scoreByIdx := make([][]int, len(mat))
+    for i:=0;i<len(mat);i++{
+        score := count(mat[i])
+        scoreByIdx[i] = []int{i, score}
+        fmt.Printf("%d : score %d\n", i, score)
+    }
+
+    sort.SliceStable(scoreByIdx, func(i,j int) bool {
+        return scoreByIdx[i][1] < scoreByIdx[j][1]
+    })
+
+    answer := make([]int, k)
+    for i:=0;i<k;i++{
+        answer[i] = scoreByIdx[i][0]
+    }
+    
+    return answer
+}
+
+func count(arr []int) int {
+    l, r := 0, len(arr)-1
+    size := -1
+
+    for l<=r {
+        mid := (l+r)/2
+
+        if arr[mid] == 1 {
+            size = mid
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    return size + 1
+}
+
+/*
+l, r = 3, 4
+
+
+
+*/
