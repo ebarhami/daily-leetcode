18da9bdfc20fba42240e56affa85ba9c2a001beb
ebarhami <31585147+ebarhami@users.noreply.github.com>
2026-06-14 20:56:52 +0700

    Time: 6 ms (48.81%) | Memory: 12.5 MB (50.00%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.go
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func pairSum(head *ListNode) int {
+    slow, fast := &ListNode{Next: head}, &ListNode{Next: head}
+
+    for fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+
+    // reverse slow.Next
+    newReversed := slow.Next
+    curr, prev, next := newReversed, &ListNode{}, &ListNode{}
+    for curr != nil {
+        next = curr.Next
+        curr.Next = prev
+        prev = curr
+
+        curr = next
+    }
+
+    max := -1
+    for prev.Next != nil {
+        sum := head.Val + prev.Val
+        if sum > max {
+            max = sum
+        }
+        head = head.Next
+        prev = prev.Next
+    }
+
+    return max
+}
+
+/*
+1 -> 2 -> 3
+
+
+
+
+*/
