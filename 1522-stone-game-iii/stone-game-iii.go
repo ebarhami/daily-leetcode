f6df80ae2bbc99bbf91c117a856cfee4bfc274ad
ebarhami <31585147+ebarhami@users.noreply.github.com>
2026-08-03 10:48:50 +0700

    Time: 4 ms (85.71%) | Memory: 8.9 MB (100.00%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.go
@@ -0,0 +1,26 @@
+func stoneGameIII(stoneValue []int) string {
+    n := len(stoneValue)
+    dp := make([]int, n+1)
+
+    for i:=n-1;i>=0;i--{
+        dp[i] = stoneValue[i] - dp[i+1]
+        if i + 2 <= n {
+            dp[i] = max(dp[i], stoneValue[i]+stoneValue[i+1]-dp[i+2])
+        }
+        if i + 3 <= n {
+            dp[i] = max(dp[i], stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3])
+        }
+    }
+    if dp[0] > 0 {
+        return "Alice"
+    } else if dp[0] < 0 {
+        return "Bob"
+    } else {
+        return "Tie"
+    }
+}
+
+func max(a, b int) int{
+    if a > b {return a}
+    return b
+}
