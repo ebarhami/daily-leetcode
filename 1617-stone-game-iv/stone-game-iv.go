0b53d183d99dad06917a76957207904756ff9a8c
ebarhami <31585147+ebarhami@users.noreply.github.com>
2026-08-10 19:17:21 +0700

    Time: 8 ms (100.00%) | Memory: 4.2 MB (100.00%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.go
@@ -0,0 +1,17 @@
+func winnerSquareGame(n int) bool {
+	dp := make([]bool, n+1)
+
+	dp[0] = false
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			if !dp[i-j*j] {
+				dp[i] = true
+
+				break
+			}
+		}
+	}
+
+	return dp[n]
+}
