2a84fe1e9ce038a66f7b9d5b312f54935954f801
ebarhami <31585147+ebarhami@users.noreply.github.com>
2026-09-02 10:28:48 +0700

    Time: 67 ms (21.35%) | Memory: 32.6 MB (19.10%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.go
@@ -0,0 +1,41 @@
+func longestPalindromeSubseq(s string) int {
+    n := len(s)
+    dp := make([][]int, n)
+    for i:=0;i<n;i++{
+        dp[i] = make([]int, n)
+        for j:=0;j<n;j++{
+            dp[i][j] = -1
+        }
+    }
+
+    return solve(dp, s, 0, n-1)
+}
+
+func solve(dp [][]int, s string, l, r int) int {
+    if l > r {
+        return 0
+    }
+    if l == r {
+        return 1
+    }
+    if dp[l][r] != -1 {
+        return dp[l][r]
+    }
+    
+    if s[l] == s[r] {
+        return 2 + solve(dp, s, l+1, r-1)
+    }
+    goLeft := solve(dp, s, l+1, r)
+    goRight := solve(dp, s, l, r-1)
+
+    dp[l][r] = goRight
+    if goLeft > goRight {
+        dp[l][r] = goLeft
+    }
+
+    return dp[l][r]
+}
+
+/*
+states: left index , right index
+*/
